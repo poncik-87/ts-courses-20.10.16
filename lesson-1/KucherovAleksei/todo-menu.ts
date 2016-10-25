ea14a3ce2ea6aa8bf24b747624d2f99954ab1e7e
Alex Kucherov <a.kucherov@nullgr.com>
2016-10-25 09:52:49 +0300

    homework done.

--- lesson-1/KucherovAleksei/todo-menu.ts
+++ lesson-1/KucherovAleksei/todo-menu.ts
@@ -1,30 +1,82 @@
-type menuList ={title: string, items: string[]}[];
+type menuList = {title: string, items?: menuList}[];
 
 let menuList: menuList = [
-    {title: 'JavaScript', items: ['React', 'Angular2', 'Cycle.js']},
-    {title: 'Dart', items: ['Flutter', 'Angular2', 'Polymer']},
+    {
+        title: 'Животные',
+        items: [
+            {
+                title: 'Млекопитающие',
+                items: [
+                    { title: 'Коровы' },
+                    { title: 'Ослы' },
+                    { title: 'Собаки' },
+                    { title: 'Тигры' }
+                ]
+            },
+            {
+                title: 'Другие',
+                items: [
+                    {
+                        title: 'Змеи',
+                        items: [
+                            { title: 'Кобра' },
+                            { title: 'Анаконда' },
+                            { title: 'Удав' },
+                        ]
+                    },
+                    { title: 'Птицы' },
+                    { title: 'Ящерицы' },
+                ],
+            },
+        ]
+    },
+    {
+        title: 'Рыбы',
+        items: [
+            {
+                title: 'Аквариумные',
+                items: [
+                    { title: 'Гуппи' },
+                    { title: 'Скалярии' }
+                ]
+            },
+            {
+                title: 'Форель',
+                items: [
+                    { title: 'Морская форель' }
+                ]
+            },
+        ]
+    }
 ];
 
-function generateMenu(list: menuList): string {
-    let z:string = `<ul>`;
-    for(let a of list){
-        z+=`<li><a class="title">${a.title}</a><ul>`
-        for(let item of a.items){
-          z+=`<li><a>${item}</a></li>`
+function generateMenu( list: menuList ): string {
+
+    let z: string = `<ul>`;
+
+    for ( let a of list ) {
+
+        if ( a.hasOwnProperty( 'items' ) ) {
+            z += `<li><a class="title">${a.title}</a>`;
+            z += generateMenu( a.items );
         }
-        z+=`</li></ul>`
+        else {
+            z += `<li><a class="item">${a.title}</a>`;
+        }
+
+        z += `</li>`;
     }
-    z+=`</ul>`;
+    z += `</ul>`;
     return z;
 }
 
-let navMenuList:HTMLDivElement = document.querySelector('.menu') as HTMLDivElement;
-navMenuList.innerHTML = generateMenu(menuList);
-navMenuList.onclick =(ev:MouseEvent)=>{
+let navMenuList: HTMLDivElement = document.querySelector( '.menu' ) as HTMLDivElement;
+navMenuList.innerHTML = generateMenu( menuList );
+navMenuList.onclick = ( ev: MouseEvent ) => {
     let el = <HTMLAnchorElement>ev.target;
     let classList = el.classList;
-    if(classList.contains('title')){
-        let parentLi =el.parentNode as HTMLLIElement;
-        parentLi.classList.toggle('menu-open')
+    if ( classList.contains( 'title' ) ) {
+        let parentLi = el.parentNode as HTMLLIElement;
+        parentLi.classList.toggle( 'menu-open' );
     }
-}
+};
